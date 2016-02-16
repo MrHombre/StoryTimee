01dea7755796f83347ca44ec9cbf4eecd3ffdaf1
James Lett <jlett89@gmail.com>
2016-02-15 17:31:03 -0800

    add more controller actions and show.html page

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -8,4 +8,24 @@ class StoriesController < ApplicationController
 		@story = Story.new
 	end
 
+	def create
+		@story = Story.new(story_params)
+		if @story.save
+			redirect_to @story
+		else
+			flash[:danger] = @story.errors.full_messages.to_sentence
+			render :new
+		end
+	end
+
+	def show
+		@story = Story.find(params[:id])
+	end
+
+	private
+
+	def story_params
+		params.require(:story).permit(:body)
+	end
+
 end
