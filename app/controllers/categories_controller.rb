60b28f1ca1ea38e9b8c4cbb620534e6df0cfba03
James Lett <jlett89@gmail.com>
2016-02-15 20:33:31 -0800

    Categories are up and working, plus a syntax error fixed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,6 @@ class CategoriesController < ApplicationController
 
 	def show
 		@category = Category.find(params[:id])
-		@stories = Story.where(category_id: params[:id].order[id: :desc])
+		@stories = Story.where(category_id: params[:id]).order([id: :desc])
 	end
 end
